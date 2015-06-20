785eed8e171a908a7caf0e37b5c8b66380d1f028
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-20 23:52:03 +0200

    take more samples

--- data-raw/correctness.R
+++ data-raw/correctness.R
@@ -6,7 +6,7 @@ devtools::load_all(pkg)
 source(file.path(pkg$path, "data-raw", "check.R"))
 
 
-N <- 5 %>% setNames(nm = .)
+N <- 5:10 %>% setNames(nm = .)
 R <- 1 %>% setNames(nm = .)
 
 correctness <- .check()
