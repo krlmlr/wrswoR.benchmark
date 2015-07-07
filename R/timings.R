765a4b567b9ef3dd81ce52c7481ee8e9fc73e268
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-07-07 21:55:30 +0200

    specs

--- R/timings.R
+++ R/timings.R
@@ -1,7 +1,7 @@
 #' Run times
 #'
 #' Run times measured on an Intel(R) Xeon(R) CPU E5-2643 clocked at 3.30 GHz
-#' with 10 MB cache.
+#' with 10 MB cache, running Debian Squeeze, R 3.1.3 and gcc 4.4.5-8.
 #'
 #' @docType data
 "timings"
