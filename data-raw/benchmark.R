0971b262f39aab4c4c2a36789f878de3164e711a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-12 22:02:25 +0100

    use prob and mix

--- data-raw/benchmark.R
+++ data-raw/benchmark.R
@@ -1,44 +1,50 @@
 set.seed(20150619L)
 
 R <- c(0.01, 0.1, 1) %>% setNames(nm = .)
-PROB <- list(uniform = function(n) rep(1, n),
-             linear = seq_len,
-             rev_linear = . %>% seq_len %>% rev,
-             shuffled_linear = sample.int)
+PROB <- setNames(nm = c("uniform", "linear", "exp"))
+MIX <- setNames(nm = c("asc", "desc", "shuffle"))
 
 .benchmark <- function() {
   plyr::ldply(
-    PROB,
-    function(probf) {
+    N,
+    function(n) {
       plyr::ldply(
-        N,
-        function(n) {
-          prob <- probf(n)
+        R,
+        function(r) {
           plyr::ldply(
-            R,
-            function(r) {
-              s <- as.integer(ceiling(n * r))
-              print(list(time=Sys.time(), n=n, s=s, prob=summary(prob)))
-              experiments <- list(
-                ccrank=quote(sample_int_crank(n, s, prob)),
-                crank=quote(sample_int_crank(n, s, prob)),
-                rank=quote(sample_int_rank(n, s, prob)),
-                rej=quote(sample_int_rej(n, s, prob)),
-                expj=quote(sample_int_expj(n, s, prob)),
-                expjs=quote(sample_int_expjs(n, s, prob)),
-                R=quote(sample_int_R(n, s, prob))
-              )
-              if (n * s >= 0.999e9) {
-                experiments[["R"]] <- NULL
-              }
-              microbenchmark::microbenchmark(
-                list = experiments,
-                control = list(order = "block", warmup = 10)
-              )
+            PROB,
+            function(prob_name) {
+              MY_MIX <- MIX
+              if (prob_name == "uniform") MIX <- MIX[1L]
+              plyr::ldply(
+                MY_MIX,
+                function(mix_name) {
+                  alpha <- 10 ** (300 / n)
+                  prob <- wrswoR.sample::mix()[[mix_name]](wrswoR.sample::prob(alpha)[[prob_name]](n))
+                  s <- as.integer(ceiling(n * r))
+                  experiments <- list(
+                    #ccrank=quote(sample_int_crank(n, s, prob)),
+                    crank=quote(sample_int_crank(n, s, prob)),
+                    rank=quote(sample_int_rank(n, s, prob)),
+                    rej=quote(sample_int_rej(n, s, prob)),
+                    expj=quote(sample_int_expj(n, s, prob)),
+                    #expjs=quote(sample_int_expjs(n, s, prob)),
+                    R=quote(sample_int_R(n, s, prob))
+                  )
+                  if (n * s >= 0.999e9) {
+                    experiments[["R"]] <- NULL
+                  }
+                  print(list(time=Sys.time(), n=n, s=s, prob=head(prob), experiments=names(experiments)))
+                  microbenchmark::microbenchmark(
+                    list = experiments,
+                    control = list(order = "block", warmup = 10)
+                  )
+                }) %>%
+                dplyr::rename(mix = .id)
             }) %>%
-            dplyr::mutate(r = as.numeric(.id), .id = NULL)
-        }) %>%
-        dplyr::mutate(n = as.integer(.id), .id = NULL)
-    }) %>%
-    dplyr::rename(prob = .id)
+          dplyr::rename(prob = .id)
+      }) %>%
+      dplyr::mutate(r = as.numeric(.id), .id = NULL)
+  }) %>%
+  dplyr::mutate(n = as.integer(.id), .id = NULL)
 }
