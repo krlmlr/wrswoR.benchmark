941eb2aebce350675a5be311e823bd75b4963772
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-12 16:18:45 +0100

    don't run R for too large experiments

--- data-raw/benchmark.R
+++ data-raw/benchmark.R
@@ -19,14 +19,20 @@ PROB <- list(uniform = function(n) rep(1, n),
             function(r) {
               s <- as.integer(ceiling(n * r))
               print(list(n=n, s=s, prob=summary(prob)))
+              experiments <- list(
+                ccrank=quote(sample_int_crank(n, s, prob)),
+                crank=quote(sample_int_crank(n, s, prob)),
+                rank=quote(sample_int_rank(n, s, prob)),
+                rej=quote(sample_int_rej(n, s, prob)),
+                expj=quote(sample_int_expj(n, s, prob)),
+                expjs=quote(sample_int_expjs(n, s, prob)),
+                R=quote(sample_int_R(n, s, prob)),
+              )
+              if (n * s >= 0.999e9) {
+                experiments[["R"]] <- NULL
+              }
               microbenchmark::microbenchmark(
-                ccrank=sample_int_crank(n, s, prob),
-                crank=sample_int_crank(n, s, prob),
-                rank=sample_int_rank(n, s, prob),
-                rej=sample_int_rej(n, s, prob),
-                expj=sample_int_expj(n, s, prob),
-                expjs=sample_int_expjs(n, s, prob),
-                R=sample_int_R(n, s, prob),
+                list = experiments,
                 control = list(order = "block", warmup = 10)
               )
             }) %>%
