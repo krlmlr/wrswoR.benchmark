0382c19f1e286906190a82056de60a7fffe17d77
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-02-12 11:12:58 +0100

    auto-space

--- R/aaa-rextdata.R
+++ R/aaa-rextdata.R
@@ -1,64 +1,64 @@
 auto_extdata <-
-function (assign.env = parent.frame()) 
-{
+  function (assign.env = parent.frame())
+  {
     extension_pattern <- "[.]rds$"
-    files <- dir(extdata_path(assign.env), pattern = extension_pattern, 
-        full.names = TRUE)
+    files <- dir(extdata_path(assign.env), pattern = extension_pattern,
+                 full.names = TRUE)
     read_rds(files, assign.env = assign.env)
-}
+  }
 
 read_rds <-
-function (..., assign.env = parent.frame()) 
-{
+  function (..., assign.env = parent.frame())
+  {
     dots <- c(...)
     if (length(dots) == 0L) {
-        return()
+      return()
     }
     unnamed <- (names(dots) == "")
     if (length(unnamed) == 0L) {
-        unnamed <- rep(TRUE, length(dots))
+      unnamed <- rep(TRUE, length(dots))
     }
     names(dots)[unnamed] <- name_from_rds(dots[unnamed])
     dots_expr <- lapply(dots, function(dot) {
-        dot <- normalizePath(dot)
-        bquote(readRDS(.(dot)))
+      dot <- normalizePath(dot)
+      bquote(readRDS(.(dot)))
     })
     delayed_assign_(.dots = dots_expr, assign.env = assign.env)
-}
+  }
 
 extdata_path <-
-function (package.env) 
-{
+  function (package.env)
+  {
     system.file(extdata_name(), package = utils::packageName(package.env))[[1L]]
-}
+  }
 
 extdata_name <-
-function () 
-c("extdata", file.path("inst", "extdata"))
+  function ()
+    c("extdata", file.path("inst", "extdata"))
 
 name_from_rds <-
-function (x) 
-{
-    if (length(x) == 0L) 
-        return()
+  function (x)
+  {
+    if (length(x) == 0L)
+      return()
     gsub("[.]rds$", "", basename(x))
-}
+  }
 
 delayed_assign_ <-
-function (..., .dots, assign.env = parent.frame()) 
-{
+  function (..., .dots, assign.env = parent.frame())
+  {
     dots <- lazyeval::all_dots(.dots, ..., all_named = TRUE)
     ret <- mapply(delayed_assign_one, names(dots), dots, MoreArgs = list(assign.env = assign.env))
     invisible(ret)
-}
+  }
 
 delayed_assign_one <-
-function (name, expr, assign.env = parent.frame()) 
-{
+  function (name, expr, assign.env = parent.frame())
+  {
     force(name)
     force(expr)
     delayedAssign(name, lazyeval::lazy_eval(expr), assign.env = assign.env)
     invisible(name)
-}
+  }
 
 auto_extdata()
