70694b76bd5de3f4e55fd574b28cff89fcb2d68a
Kirill Müller <krlmlr+r@mailbox.org>
2017-04-14 16:37:24 +0200

    oops

--- data-raw/timings_sort.R
+++ data-raw/timings_sort.R
@@ -7,6 +7,6 @@ source(file.path(pkg$path, "data-raw", "benchmark_sort.R"))
 
 N <- trunc(10 ** seq(1.5, 6, by = 0.5)) %>% setNames(nm = .)
 
-timings_sort <- .benchmark()
+timings_sort <- .benchmark_sort()
 
 rextdata::use_extdata(timings_sort, overwrite = TRUE, compress = "xz")
